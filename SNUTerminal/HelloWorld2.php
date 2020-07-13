e9565d5b5f70ff3699f594067cf1700227f5015f
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-12 20:33:24 -0700

    2 files
    2 files for this directory, in PHP, and HTML5 formats.

---
+++ SNUTerminal/HelloWorld2.php
@@ -0,0 +1,10 @@
+<?php // hello world PHP v2
+// start of script
+/* definitions */.
+$greet1 = "hello"; // greeting
+$planet1 = "world"; // planet
+/* echo chamber */
+echo ("$greet1 $planet1"); // echos "hello" ($greet1) "world" ($planet1)
+/* end of echo chamber */
+print ("hello world"); // simplified version to print hello world in PHP
+// end of script?>
