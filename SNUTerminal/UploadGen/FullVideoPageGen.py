edfb38ad3a9b08f923c5c086c74b29098bfc7e32
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-12 20:32:50 -0700

    2 files
    2 files for this directory, in Python format.

---
+++ SNUTerminal/UploadGen/FullVideoPageGen.py
@@ -0,0 +1,38 @@
+# SNU video page script
+# for videos of type "full"
+# full videos play on a separate page, other videos can be played with other videos on the same page
+'''
+Written in Python
+SNU Vidoe generator file
+'''
+# start of script
+''' definitions '''
+# Strings
+url = str
+title1 = str
+description1 = str
+# Integers
+viewCount = int
+# Floats 
+'''likeDislikeRatio = float'''
+likeCount = float
+dislikeCount = float
+""" end of definitions """
+''' functions '''
+''' test values '''
+likeCount == 42.00
+dislikeCount == 42.00
+""" end of test values """
+def ratioCalcLD():
+	likeDislikeRatio1 = float()
+	countItLD == float(likeCount == (likeCount + dislikeCount))
+	positiveLD == float(countItLD == countItLD / likeCount)
+	print ("Positive Like/Dislike ratio: " + str(positiveLD))
+""" end of functions """
+# testing
+editFileNO = str(input("You are editing the main video page generator file. Editing this file can disrupt the creation of new videos. Be careful here"))
+print ("Hello Internet!")
+print (ratioCalcLD())
+noMore = str(input("Press [ENTER] key to exit this files script terminal"))
+# end of script
+''' This script is pissing me off, as I spent too much time away from python. I will continue working on it another time '''
